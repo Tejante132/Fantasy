f7c247c5fdad1770cdc89951b8612b3ebd20267c
Nory09 <noraylanne.b2004@gmail.com>
2025-12-26 18:12:55 +0100

    modifs programme pt & tableau pt + début script concordance pt

---
+++ programmes/concordance_pt.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/bash
+FICHIER_URLS=$1
+if [ $# -ne 1 ]
+then
+	echo "Ce script demande 1 argument : Le chemin du fichier d'urls"
+	exit
+fi
+
+NB_LIGNES=1
+while read -r line;
+do
+echo -e "<html>
+<head>
+	<meta charset=\"UTF-8\">
+	<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css\">
+</head>
+<body>
+	<div class=\"content is-large\">
+		<h1 class=\"title is-1 has-text-centered\">Concordancier</h1>
+	</div>
+	<table class = \"table is-bordered is-hoverable is-fullwidth\">
+		<tr class=\"is-link\">
+			<th>Contexte Gauche</th>
+			<th>Mot</th>
+			<th>Contexte Droit</th>
+		</tr>" >> "../concordance/pt/concordancier$NB_LIGNES.html"
+lynx -dump -nolist ${line} | grep -E -B2 -i "fantasias?" >> "./temp_concordance_gauche$NB_LIGNES.txt"
+lynx -dump -nolist ${line} | grep -E -A2 -i "fantasias?" >> "./temp_concordance_droite$NB_LIGNES.txt"
+lynx -dump -nolist ${line} | grep -E -i "fantasias?" >> "./temp_mot$NB_LIGNES.txt"
+done
+
+
+
+
+#Éléments concordance :
+#lien vers le tableau : <td><a href=\"../concordance/pt/concordancier$NB_LIGNES.html\"></a></td>
+#création du tableau : echo -e "<html>
+	#<head>
+		#<meta charset=\"UTF-8\">
+		#<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css\">
+	#</head>
+	#<body>
+		#<div class=\"content is-large\">
+			#<h1 class=\"title is-1 has-text-centered\">Concordancier $NB_LIGNES</h1>
+		#</div>
+		#<table class = \"table is-bordered is-hoverable is-fullwidth\">
+			#<tr class=\"is-link\">
+				#<th>Contexte Gauche</th>
+				#<th>Mot</th>
+				#<th>Contexte Droit</th>
+			#</tr>"
+#récup cg : CONCORDANCE_GAUCHE=$(lynx -dump -nolist ${line} | grep -E -B2 -i "fantasias?")
+#récup cd : CONCORDANCE_DROIT=$(lynx -dump -nolist ${line} | grep -E -A2 -i "fantasias?")
+#récup mot : MOT=$(lynx -dump -nolist ${line} | grep -E -i "fantasias?")
+#remplissage du tableau : echo -e "		<tr>
+		#<td>$CONCORDANCE_GAUCHE</td>
+		#<td>$MOT</td>
+		#<td>$CONCORDANCE_DROIT</td>
+	#</tr>" >> "./concordance/pt/concordancier$NB_LIGNES.html"
+# finition du tableau : echo -e	"</table>
+	#</body>
+	#</html>" >> "./concordance/pt/concordancier$NB_LIGNES.html"
+
--- programmes/projet-pt.sh
+++ programmes/projet-pt.sh
@@ -16,7 +16,7 @@ echo -e "<html>
 </head>
 <body>
 	<div class=\"content is-large\">
-		<h1 class=\"title is-1 has-text-centered\">Mini Projet : Résultats</h1>
+		<h1 class=\"title is-1 has-text-centered\">Projet pt : Résultats</h1>
 	</div>
 	<table class = \"table is-bordered is-hoverable is-fullwidth\">
 		<tr class=\"is-link\">
@@ -56,10 +56,10 @@ do
 		<td>${line}</td>
 		<td>$CODE_HTTP</td>
 		<td>$ENCODAGE</td>
-		<td><a href=\"./aspirations/pt/aspiration$NB_LIGNES.txt\"></a></td>
-		<td><a href=\"./dumps/pt/dump$NB_LIGNES.txt\"></a></td>
+		<td><a href=\"../aspirations/pt/aspiration$NB_LIGNES.txt\">voir aspiration</a></td>
+		<td><a href=\"../dumps/pt/dump$NB_LIGNES.txt\">voir dump</a></td>
 		<td>$N_MOTS</td>
-		<td><a href=\"./contextes/pt/contextes$NB_LIGNES.txt\"></a></td>
+		<td><a href=\"../contextes/pt/contextes$NB_LIGNES.txt\">voir contextes</a></td>
 	</tr>" >> "$FICHIER_SORTIE";
 	NB_LIGNES=$(expr $NB_LIGNES + 1);
 done < "$FICHIER_URLS";
@@ -69,31 +69,3 @@ echo -e "	</table>
 </html>" >> "$FICHIER_SORTIE"
 
 
-#Éléments concordance :
-#lien vers le tableau : <td><a href=\"../concordance/pt/concordancier$NB_LIGNES.html\"></a></td>
-#création du tableau : echo -e "<html>
-	#<head>
-		#<meta charset=\"UTF-8\">
-		#<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css\">
-	#</head>
-	#<body>
-		#<div class=\"content is-large\">
-			#<h1 class=\"title is-1 has-text-centered\">Concordancier $NB_LIGNES</h1>
-		#</div>
-		#<table class = \"table is-bordered is-hoverable is-fullwidth\">
-			#<tr class=\"is-link\">
-				#<th>Contexte Gauche</th>
-				#<th>Mot</th>
-				#<th>Contexte Droit</th>
-			#</tr>"
-#récup cg : CONCORDANCE_GAUCHE=$(lynx -dump -nolist ${line} | grep -E -B2 -i "fantasias?")
-#récup cd : CONCORDANCE_DROIT=$(lynx -dump -nolist ${line} | grep -E -A2 -i "fantasias?")
-#récup mot : MOT=$(lynx -dump -nolist ${line} | grep -E -i "fantasias?")
-#remplissage du tableau : echo -e "		<tr>
-		#<td>$CONCORDANCE_GAUCHE</td>
-		#<td>$MOT</td>
-		#<td>$CONCORDANCE_DROIT</td>
-	#</tr>" >> "./concordance/pt/concordancier$NB_LIGNES.html"
-# finition du tableau : echo -e	"</table>
-	#</body>
-	#</html>" >> "./concordance/pt/concordancier$NB_LIGNES.html"
